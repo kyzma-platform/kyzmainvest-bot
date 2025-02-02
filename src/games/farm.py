8e734158268a9035fb7368bb79397b355a03f0b9
nazar-sulima <nazarukrainian47@gmail.com>
2025-02-02 17:44:18 +0000

    fix farm

--- src/games/farm.py
+++ src/games/farm.py
@@ -6,10 +6,15 @@ from os import getenv
 
 from bot.bot_replies import bot_replies
 
+from database import MongoDB
+
 class Farm:
     def __init__(self):
         self.bot_replies = bot_replies
         self.bot = telebot.TeleBot(getenv("BOT_TOKEN"))
+        self.database = MongoDB()
+        self.budget = 5587251063
+        self.owner = getenv("ADMIN_ID")
         self.farm_rare_coins = 600
         self.farm_rare_chance = 0.1
         self.tax = 0.4
@@ -36,13 +41,16 @@ class Farm:
                     coins_tax = round(coins * self.tax)
                     coins_after_tax = coins - coins_tax
                 print(coins_after_tax)
+                budget = self.database.find_user_id(self.budget)
+                budget['coins'] += coins_tax
                 user['coins'] += coins_after_tax
                 user['last_farm_time'] = current_time
                 print(f"User {user['nickname']} farmed {coins} coins. Total: {user['coins']}")
                 self.bot.reply_to(message,
                     f"Вы заработали {coins} KyZmaCoin\n"
                     f"Налог: {coins_tax} KyZmaCoin\n"
-                    f"Зарплата после налога: {coins - coins_tax} KyZmaCoin\n" 
+                    f"Зарплата после налога: {coins_after_tax} KyZmaCoin\n" 
                     f"Итоговая сумма: {user['coins']} KyZmaCoin\n"
                     f"Вы сможете снова фармить через 1 час")
+                self.bot.send_message(self.owner, f"{coins_tax} added to budget.\n Budget:{budget['coins']}")
                 return user
